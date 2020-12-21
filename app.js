6492b57861b4e994ffacbbcdce01ce9d1093abae
shamsuzzamansadi <abmsadi06@gmail.com>
2020-12-21 10:09:46 +0100

    removed the findname and using separate first and lastname

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const faker = require ('faker');
+
 let firstName = faker.name.firstName();
 let lastName = faker.name.lastName();
-let name = faker.name.findName();
-console.log(`name: ${name}`);
+
 console.log(`firstname: ${firstName}`);
-console.log(`lastname: `)
+console.log(`lastname: ${lastName}`)
 let emailAddress = `${firstName}` + `${lastName}` + `${Math.random()}`  + '@mailbox.org';
 
 console.log('emailAddress: ',emailAddress);
